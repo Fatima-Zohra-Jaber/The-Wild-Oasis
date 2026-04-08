492fe4e85ea405e36ec4be68523bccf47b3c2c88
Fatima Zohra Jaber <jaber.fatiima.zohra@gmail.com>
2026-04-08 14:45:17 +0100

    Fix sidebar size on all screens

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -18,8 +18,8 @@ function Sidebar() {
   ];
 
   return (
-    <aside className="p-6">
-      <img src={logo} alt="App logo" className="w-28 mb-6" />
+    <aside className="p-6 w-52 2xl:w-65">
+      <img src={logo} alt="App logo" className="w-28 2xl:w-32 mb-6 mx-auto" />
 
       <nav className="flex flex-col gap-2">
         {navItems.map(({ to, label, icon: Icon }) => (
@@ -28,17 +28,17 @@ function Sidebar() {
             to={to}
             className={({ isActive }) =>
               `flex items-center gap-2 px-4 py-2 rounded font-semibold text-stone-700 transition-colors 
-              ${isActive ? "bg-gray-100" : " hover:bg-gray-50"}`
+              ${isActive ? "bg-gray-100" : "hover:bg-gray-50"}`
             }
           >
             {({ isActive }) => (
               <>
                 <Icon
-                  className={`text-lg ${
+                  className={`text-lg 2xl:text-xl ${
                     isActive ? "text-purple-800/90" : "text-gray-500"
                   }`}
                 />
-                <span className="text-sm">{label}</span>
+                <span className="text-sm 2xl:text-md">{label}</span>
               </>
             )}
           </NavLink>

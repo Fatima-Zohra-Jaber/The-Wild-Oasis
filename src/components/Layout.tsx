e65c51783b6fa2f326e197e66105e390641dabfe
Fatima Zohra Jaber <jaber.fatiima.zohra@gmail.com>
2026-04-08 17:43:51 +0100

    Fix Outlet size on all screens

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,10 +6,12 @@ function Layout() {
   return (
     <div className="flex h-screen overflow-hidden">
       <Sidebar />
-      <div className="flex flex-col flex-1">
+      <div className="flex flex-1 flex-col">
         <Header />
-        <main className="flex-1 overflow-y-auto  bg-gray-50 p-6">
-          <Outlet />
+        <main className="flex-1 overflow-y-auto bg-gray-50 p-6">
+          <div className="mx-auto max-w-7xl">
+            <Outlet />
+          </div>
         </main>
       </div>
     </div>

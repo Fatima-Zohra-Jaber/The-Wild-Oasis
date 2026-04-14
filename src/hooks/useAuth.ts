0c1cd90423fafbe161a22b1d133adca65220a3e5
Fatima Zohra Jaber <jaber.fatiima.zohra@gmail.com>
2026-04-14 10:57:25 +0100

    implement user creation

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,7 +1,12 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
-import { getCurrentUser, login as loginAPi, logout as logoutApi } from "../services/apiAuth";
+import {
+  getCurrentUser,
+  login as loginAPi,
+  logout as logoutApi,
+  signup as signupAPI,
+} from "../services/apiAuth";
 
 export function useLogin() {
   const queryClient = useQueryClient();
@@ -11,7 +16,7 @@ export function useLogin() {
     mutationFn: (credentials: { email: string; password: string }) =>
       loginAPi(credentials),
     onSuccess: (user) => {
-      queryClient.setQueryData(['user'], user.user); // Update the user data in the cache
+      queryClient.setQueryData(["user"], user.user); // Update the user data in the cache
       navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
@@ -31,19 +36,33 @@ export function useUser() {
 }
 
 export function useLogout() {
-    const queryClient = useQueryClient();
-    const navigate = useNavigate();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
-    const { mutate: logout , isPending: isLoading } = useMutation({
-        mutationFn: logoutApi,
-        onSuccess: () => {
-            queryClient.removeQueries(); 
-            navigate("/login", { replace: true });
-        },
-        onError: (err) => {
-            console.log("ERROR", err);
-            toast.error("Failed to log out. Please try again.");
-        },
-    });
-    return { logout, isLoading };
+  const { mutate: logout, isPending: isLoading } = useMutation({
+    mutationFn: logoutApi,
+    onSuccess: () => {
+      queryClient.removeQueries();
+      navigate("/login", { replace: true });
+    },
+    onError: (err) => {
+      console.log("ERROR", err);
+      toast.error("Failed to log out. Please try again.");
+    },
+  });
+  return { logout, isLoading };
+}
+
+export function useSignup() {
+  const { mutate: signup, isPending: isLoading } = useMutation({
+    mutationFn: signupAPI,
+    onSuccess: () => {
+      toast.success("Account successfully created! Please log in.");
+    },
+    onError: (err) => {
+      console.log("ERROR", err);
+      toast.error(err.message || "Failed to sign up. Please try again.");
+    },
+  });
+  return { signup, isLoading };
 }
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,8 +1,128 @@
+import { useForm, useWatch } from "react-hook-form";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import { useSignup } from "../hooks/useAuth";
+import { useEffect } from "react";
 
-function Users() {
-  return (
-    <div>Users</div>
-  )
+interface User {
+  fullName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
 }
 
-export default Users
+function Users() {
+  const { signup, isLoading } = useSignup();
+  const {
+    register,
+    handleSubmit,
+    control,
+    trigger,
+    // getValues,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+  const password = useWatch({ name: "password", control });
+
+  useEffect(() => {
+    trigger("confirmPassword");
+  }, [password, trigger]);
+
+  const onSubmit = (data: User) => {
+    signup(
+      {
+        fullName: data.fullName,
+        email: data.email,
+        password: data.password,
+      },
+      { onSuccess: () => reset() },
+    );
+  };
+
+  return (
+    <>
+      <h1 className="mb-6 text-2xl font-medium text-stone-800">
+        Create a new user
+      </h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full max-w-2xl rounded-lg bg-white p-6"
+      >
+        <Input
+          label="Full name"
+          type="text"
+          placeholder="e.g. John Doe"
+          {...register("fullName", { required: "This field is required" })}
+          error={errors.fullName?.message}
+          disabled={isLoading}
+        />
+        <Input
+          label="Email"
+          type="text"
+          placeholder="e.g. john@example.com"
+          {...register("email", {
+            required: "This field is required",
+            pattern: {
+              value: /^\S+@\S+\.\S+$/i,
+              message: "Invalid email address",
+            },
+          })}
+          error={errors.email?.message}
+          disabled={isLoading}
+        />
+        <Input
+          label="Password"
+          type="password"
+          placeholder="••••••••"
+          {...register("password", {
+            required: "This field is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters",
+            },
+          })}
+          error={errors.password?.message}
+        />
+        <Input
+          label="Confirm Password"
+          type="password"
+          placeholder="••••••••"
+          {...register("confirmPassword", {
+            required: "This field is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters",
+            },
+            validate: (value) =>
+              // value === getValues("password") || "Passwords do not match",
+              value === password || "Passwords do not match",
+          })}
+          error={errors.confirmPassword?.message}
+          disabled={isLoading}
+        />
+        <div className="flex items-center justify-end gap-3">
+          <Button
+            variant="secondary"
+            type="reset"
+            onClick={() => {}}
+            isSubmitting={isLoading}
+          >
+            Cancel
+          </Button>
+          <Button variant="primary" type="submit" isSubmitting={isLoading}>
+            Create User
+          </Button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default Users;
